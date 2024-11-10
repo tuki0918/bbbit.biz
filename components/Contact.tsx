7c14e3d0b1c170735c2929cb52665848e7a929ac
Y.Yamamoto <tuki0918@gmail.com>
2024-11-10 13:08:41 +0900

    feat: update Contact component with Japanese translation and additional instructions

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -11,15 +11,23 @@ export default function Hero() {
 			className="flex-grow pt-24 pb-16 bg-gray-50"
 		>
 			<div className="container mx-auto px-4">
-				<h1 className="text-4xl font-bold mb-8 text-center">CONTACT</h1>
+				<h1 className="text-4xl font-bold mb-8">お問い合わせ</h1>
 
 				<div className="bg-white rounded-lg shadow-sm p-8">
+					<section className="border-b pb-6 mb-6">
+						<p className="text-gray-600 leading-relaxed">
+							下記フォームにご入力ください。
+							<br />※
+							ご記入漏れがある場合、お問い合わせにお答えできない場合がございます。
+						</p>
+					</section>
+
 					<iframe
 						className="mx-auto"
 						title="お問い合わせ"
 						src="https://docs.google.com/forms/d/e/1FAIpQLScBG5HiL4CFRe9LMaYC3Fvs3DKHcYAK-yXYEZLTZ9joRL8uNw/viewform?embedded=true"
 						width="640"
-						height="640"
+						height="720"
 						frameBorder={0}
 						marginHeight={0}
 						marginWidth={0}
