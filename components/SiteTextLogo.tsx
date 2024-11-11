b8811b510136f6c413e42f7f2f039ddf9016b3c9
Y.Yamamoto <tuki0918@gmail.com>
2024-11-11 22:28:33 +0900

    feat: enhance SiteTextLogo component with dark mode support

--- components/SiteTextLogo.tsx
+++ components/SiteTextLogo.tsx
@@ -1,10 +1,16 @@
 import { sourGummy } from "@/app/fonts";
+import { cn } from "@/lib/utils";
 import type { FC } from "react";
 
-const SiteTextLogo: FC = () => {
+const SiteTextLogo: FC<{
+	isDark?: boolean;
+}> = ({ isDark }) => {
 	return (
 		<span
-			className={`${sourGummy.className} font-bold text-4xl text-yellow-400`}
+			className={cn(sourGummy.className, "font-bold text-4xl", {
+				"text-yellow-400": !isDark,
+				"text-white": isDark,
+			})}
 		>
 			bbbit
 		</span>
