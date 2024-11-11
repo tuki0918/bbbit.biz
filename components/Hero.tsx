ca351f47b0c9a9f7fe97009dd1c644a0f0f99dcd
Y.Yamamoto <tuki0918@gmail.com>
2024-11-11 23:04:50 +0900

    feat: update Hero component to use dynamic site name and enhance styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,22 +1,33 @@
 "use client";
 
+import { sourGummy } from "@/app/fonts";
+import { SITE_NAME } from "@/config/app";
+import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
-import { ArrowDown } from "lucide-react";
+import { CircleArrowDown } from "lucide-react";
+import { Link } from "react-scroll";
 
 export default function Hero() {
 	return (
-		<section className="h-screen relative flex items-center justify-center bg-white overflow-hidden">
+		<section className="h-screen relative flex items-center justify-center bg-amber-400 overflow-hidden">
 			<motion.div
 				initial={{ opacity: 0, y: 20 }}
 				animate={{ opacity: 1, y: 0 }}
 				transition={{ duration: 0.8 }}
 				className="text-center z-10"
 			>
-				<h1 className="text-6xl font-bold mb-6 bg-gradient-to-r from-yellow-400 to-yellow-600 bg-clip-text text-transparent">
-					創造と革新
+				<h1
+					className={cn(
+						sourGummy.className,
+						"text-6xl font-bold text-orange-950 mb-6",
+					)}
+				>
+					{SITE_NAME}
 				</h1>
-				<p className="text-xl text-gray-600 mb-8">
-					未来を見据えた価値創造のパートナー
+				<p className="mb-12 w-3/4 md:w-auto mx-auto">
+					私達は、製品やサービス通じてお客様の日常が楽しくなるような「
+					<span className="font-bold text-xl">ビビビ</span>
+					」をお届けします。
 				</p>
 			</motion.div>
 
@@ -31,13 +42,15 @@ export default function Hero() {
 				}}
 				className="absolute bottom-8"
 			>
-				<ArrowDown className="w-8 h-8 text-yellow-500" />
+				<Link
+					to="business"
+					smooth={true}
+					duration={500}
+					className="hover:cursor-pointer"
+				>
+					<CircleArrowDown className="w-12 h-12 text-orange-950" />
+				</Link>
 			</motion.div>
-
-			<div className="absolute inset-0">
-				<div className="absolute top-0 left-0 w-96 h-96 bg-yellow-200 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob" />
-				<div className="absolute top-0 right-0 w-96 h-96 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000" />
-			</div>
 		</section>
 	);
 }
