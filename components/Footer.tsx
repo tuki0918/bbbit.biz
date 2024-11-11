a1d9164761a3ec0fb85fc6d8d84e7f56360fcf63
Y.Yamamoto <tuki0918@gmail.com>
2024-11-12 06:13:22 +0900

    feat: enhance Footer component with additional icons for navigation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,7 @@
 import { geistSans, sourGummy } from "@/app/fonts";
 import { SITE_DESCRIPTION, SITE_NAME, SITE_NAME_JP } from "@/config/app";
 import { cn } from "@/lib/utils";
+import { BookOpen, Store } from "lucide-react";
 import Link from "next/link";
 
 export default function Footer() {
@@ -18,8 +19,8 @@ export default function Footer() {
 							</Link>
 						</h3>
 						<p className="text-sm text-gray-600 mb-4">{SITE_DESCRIPTION}</p>
-						<p className="text-sm text-gray-600 mb-4">
-							<Link href="/">
+						<p className="text-sm text-gray-600 mb-4 space-x-2 flex">
+							<Link href="/" title="X">
 								<span className="[&>svg]:h-6 [&>svg]:w-6 [&>svg]:fill-black inline-block">
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
 										<title>X</title>
@@ -27,6 +28,12 @@ export default function Footer() {
 										<path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
 									</svg>
 								</span>
+							</Link>
+							<Link href="/" title="note">
+								<BookOpen />
+							</Link>
+							<Link href="/" title="shop">
+								<Store />
 							</Link>
 						</p>
 					</div>
