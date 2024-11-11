86ecf42273cfe0e6f1acf318f562c1f0416f795b
Y.Yamamoto <tuki0918@gmail.com>
2024-11-12 03:49:24 +0900

    feat: add SVG icon link to Footer component for enhanced visual appeal

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,6 +20,17 @@ export default function Footer() {
 							<Link href="/">{SITE_NAME}</Link>
 						</h3>
 						<p className="text-sm text-gray-600 mb-4">{SITE_DESCRIPTION}</p>
+						<p className="text-sm text-gray-600 mb-4">
+							<Link href="/">
+								<span className="[&>svg]:h-6 [&>svg]:w-6 [&>svg]:fill-black inline-block">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+										<title>X</title>
+										{/* <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+										<path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+									</svg>
+								</span>
+							</Link>
+						</p>
 					</div>
 
 					<div className="col-span-1">
