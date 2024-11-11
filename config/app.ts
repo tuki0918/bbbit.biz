56fe12d3deba31a13812d5ffe5a152fe4d9ae442
Y.Yamamoto <tuki0918@gmail.com>
2024-11-11 22:28:05 +0900

    Add app config

---
+++ config/app.ts
@@ -0,0 +1,5 @@
+export const SITE_NAME = "bbbit";
+export const SITE_NAME_JP = "ビビビット";
+export const SITE_DESCRIPTION =
+	"ビビビットは、製品やサービス通じてお客様の日常が楽しくなるような「ビビビ」をお届けします。";
+export const SITE_DOMAIN = "bbbit.biz";
